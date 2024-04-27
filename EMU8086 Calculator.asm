38c20ed2e88d1449b3c849ad72b462247624e00a
Joseph Maina <mainajoseph620@gmail.com>
2024-04-27 17:25:16 +0300

    Update EMU8086 Calculator.asm

--- EMU8086 Calculator.asm
+++ EMU8086 Calculator.asm
@@ -1,287 +1,292 @@
 .model small
 
- org 100h 
- 
- jmp start
+org 100h
 
-msg: db 0dh,0ah,"JOSEPH MAINA CALCULATOR",0dh,0ah,0dh,0ah,"1-For addition",0dh,0ah,"2-For multiplication",0dh,0ah,"3-For subtraction",0dh,0ah,"4-For division",0dh,0ah,"5-For square",0dh,0ah,"6-For power",0dh,0ah,"$"          
-msgA: db 0dh,0ah,"Enter Your Operation (1 to 6): $" 
-msgB: db 0dh,0ah,"Enter First Number: $"  
-msgC: db 0dh,0ah,"Enter Second Number: $" 
+jmp start
+
+msg: db 0dh,0ah,"JOSEPH MAINA CALCULATOR",0dh,0ah,0dh,0ah,"1-For addition",0dh,0ah,"2-For multiplication",0dh,0ah,"3-For subtraction",0dh,0ah,"4-For division",0dh,0ah,"5-For square",0dh,0ah,"6-For power",0dh,0ah,"$"
+msgA: db 0dh,0ah,"Enter Your Operation (1 to 6): $"
+msgB: db 0dh,0ah,"Enter First Number: $"
+msgC: db 0dh,0ah,"Enter Second Number: $"
 msgD: db 0dh,0ah,"PLEASE ENTER A VALID OPERATION $"
 msgE: db 0dh,0ah,"Result", 0dh,0ah,"the sum is:","$"
 msgF: db 0dh,0ah,"Result", 0dh,0ah,"the difference is: ","$"
 msgG: db 0dh,0ah,"Result", 0dh,0ah," the quotient is:","$"
-msgH: db 0dh,0ah,"Result", 0dh,0ah,"the answer is: " ,0dh,0ah,"$"  
-msgI: db 0dh,0ah,"Result", 0dh,0ah,"the answer is: ",0dh,0ah,"$"  
-msgJ:db 0dh,0ah,"Enter the Base ","$"  
-msgK:db 0dh,0ah,"Enter the Power","$"   
-msgM:db 0dh,0ah,"Enter The Number ","$"  
+msgH: db 0dh,0ah,"Result", 0dh,0ah,"the answer is: " ,0dh,0ah,"$"
+msgI: db 0dh,0ah,"Result", 0dh,0ah,"the answer is: ",0dh,0ah,"$"
+msgJ: db 0dh,0ah,"Enter the Base ","$"
+msgK: db 0dh,0ah,"Enter the Power","$"
+msgM: db 0dh,0ah,"Enter The Number ","$"
 msgL: db 0dh,0ah,"THANK YOU FOR USING MY CALCULATOR. @copywrite: Joseph Maina Kinyua,J17-2574-2018$"
+
 base db ?
-pow db ?  
-nl1 db    0ah,0dh,"enter base:","$"
-nl2 db   0ah,0dh," Result in power 3: ",0dh,0ah,"$"
+pow db ?
+nl1 db 0ah,0dh,"enter base:","$"
+nl2 db 0ah,0dh," Result in power 3: ",0dh,0ah,"$"
 
 .code
-start: mov ah,9
-       mov dx, offset msg 
-       int 21h   
-       mov dx, offset msgA
-       int 21h
-       mov ah,0
-       int 16h
-       cmp al,31h
-       je addition
-       cmp al,32h
-       je multiplication
-       cmp al,33h
-       je subtraction
-       cmp al,34h
-       je division 
-       cmp al,35h
-       je square 
-       cmp al,36h
-       je power
-       mov ah,9 
-       mov dx,offset msgD
-       int 21h
-       mov ah,0
-       int 16h
-       jmp start
-       
-addition: ; performing addition
-           mov ah,09h
-           mov dx, offset msgB
-           int 21h
-           mov cx,0
-           call inputno  
-           push dx
-           mov ah,9 
-           mov dx,offset msgC
-           int 21h
-           mov cx,0
-           call inputno
-           pop bx
-           add dx,bx
-           push dx
-           mov ah,9
-           mov dx,offset msgE
-           int 21h
-           pop dx
-           mov cx,10000 
-           call view
-           jmp exit
+start:
+    mov ah, 9
+    mov dx, offset msg
+    int 21h
 
-exit:     mov dx,offset msgL
-          mov ah,9
-          int 21h
-          mov ah,0
-          int 16h
-          ret 
-           
-view:      mov ax,dx
-           mov dx,0 
-           div cx
-           call viewno
-           mov bx,dx
-           mov dx,0
-           mov ax,cx
-           mov cx,10
-           div cx  
-           mov dx,bx
-           mov cx,ax
-           cmp ax,0
-           jne view
-           ret
-   
-inputno: mov ah,0
-         int 16h 
-         mov dx,0
-         mov bx,1
-         cmp al,0dh
-         je formno
-         sub ax,30h
-         call viewno
-         mov ah,0
-         push ax
-         inc cx
-         jmp inputno 
-         
-formno: pop ax
-         push dx
-         mul bx
-         pop dx
-         add dx,ax
-         mov ax,bx
-         mov bx,10  
-         push dx
-         mul bx 
-         pop dx
-         mov bx,ax
-         dec cx
-         cmp cx,0
-         jne formno 
-         ret
+    mov dx, offset msgA
+    int 21h
+    mov ah, 0
+    int 16h
 
-             
-viewno:  push ax  
-           push dx
-           mov dx,ax
-           add dl,30h
-           mov ah,2
-           int 21h 
-           pop dx
-           pop ax
-           ret 
-           
- 
-multiplication:;performing multiplication
-           mov ah,09h
-           mov dx, offset msgB
-           int 21h
-           mov cx,0
-           call inputno  
-           push dx
-           mov ah,9 
-           mov dx,offset msgC
-           int 21h
-           mov cx,0
-           call inputno
-           pop bx
-           mov ax,dx
-           mul bx
-           mov dx,ax 
-           push dx
-           mov ah,9
-           mov dx,offset msgH
-           int 21h
-           pop dx
-           mov cx,10000 
-           call view
-           jmp exit 
-           
-           
-           
-subtraction:; performing subtraction
-           mov ah,09h
-           mov dx, offset msgB
-           int 21h
-           mov cx,0
-           call inputno  
-           push dx
-           mov ah,9 
-           mov dx,offset msgC
-           int 21h
-           mov cx,0
-           call inputno
-           pop bx
-           sub bx,dx  
-           mov dx,bx
-           push dx
-           mov ah,9
-           mov dx,offset msgF
-           int 21h
-           pop dx
-           mov cx,10000 
-           call view
-           jmp exit
-           
-           
-division:;performing division
-           mov ah,09h
-           mov dx, offset msgB
-           int 21h
-           mov cx,0
-           call inputno  
-           push dx
-           mov ah,9 
-           mov dx,offset msgC
-           int 21h
-           mov cx,0
-           call inputno
-           pop bx
-           mov ax,bx
-           mov cx,dx
-           mov dx,0
-           div cx
-           mov dx,ax 
-           push dx
-           mov ah,9
-           mov dx,offset msgG
-           int 21h
-           pop dx
-           mov cx,10000 
-           call view
-           jmp exit 
-           
-square:;performing square
-           mov ah,09h
-           mov dx, offset msgM
-           int 21h
-           mov cx,0
-           call inputno  
-           push dx
-           mov ah,9
-           mov cx,0 
-           pop bx
-           mov ax,dx
-           mul bx
-           mov dx,ax 
-           push dx
-           mov ah,9
-           mov dx,offset msgI
-           int 21h
-           pop dx
-           mov cx,10000 
-           call view
-           jmp exit 
-main proc
-            mov ax,@data
-            mov ds,ax                     
-power:;performing power
-      ;enter_base  
-      lea dx,nl1
-           mov ah,09h
-           mov dx, offset msgJ
-           int 21h 
-           mov ah,01h
-           int 21h
-           sub al,30h
-           mov bl,al
-           mov base,al 
-           ;enter_power
-           lea dx,nl2
-           mov ah,09h
-           mov dx, offset msgK
-           int 21h
-           mov ah,01h
-           int 21h
-           sub al,30h
-           mov cl,al
-           dec  cl
-           mov ax,00
-           mov al,base 
- lbl1:   
-     mul bl
-     loop lbl1
-     mov cl,10
-     div cl
-     add ax,3030h
-     mov dx,ax 
-     mov ah,02h
-     int 21h
-     mov dl,dh
-     int 21h
-     mov ah,4ch
-     int 21h
-     
-   
-  
+    cmp al, '1'
+    je addition
+    cmp al, '2'
+    je multiplication
+    cmp al, '3'
+    je subtraction
+    cmp al, '4'
+    je division
+    cmp al, '5'
+    je square
+    cmp al, '6'
+    je power
 
- ret 
- main endp
+    mov ah, 9
+    mov dx, offset msgD
+    int 21h
+
+    mov ah, 0
+    int 16h
+    jmp start
+
+addition:
+    ; Performing addition
+    mov ah, 9
+    mov dx, offset msgB
+    int 21h
+    mov cx, 0
+    call inputno
+    push dx
+    mov ah, 9
+    mov dx, offset msgC
+    int 21h
+    mov cx, 0
+    call inputno
+    pop bx
+    add dx, bx
+    push dx
+    mov ah, 9
+    mov dx, offset msgE
+    int 21h
+    pop dx
+    mov cx, 10000
+    call view
+    jmp exit
+
+exit:
+    mov dx, offset msgL
+    mov ah, 9
+    int 21h
+    mov ah, 0
+    int 16h
+    ret
+
+view:
+    mov ax, dx
+    mov dx, 0
+    div cx
+    call viewno
+    mov bx, dx
+    mov dx, 0
+    mov ax, cx
+    mov cx, 10
+    div cx
+    mov dx, bx
+    mov cx, ax
+    cmp ax, 0
+    jne view
+    ret
+
+inputno:
+    mov ah, 0
+    int 16h
+    mov dx, 0
+    mov bx, 1
+    cmp al, 0dh
+    je formno
+    sub al, 30h
+    call viewno
+    mov ah, 0
+    push ax
+    inc cx
+    jmp inputno
+
+formno:
+    pop ax
+    push dx
+    mul bx
+    pop dx
+    add dx, ax
+    mov ax, bx
+    mov bx, 10
+    push dx
+    mul bx
+    pop dx
+    mov bx, ax
+    dec cx
+    cmp cx, 0
+    jne formno
+    ret
+
+viewno:
+    push ax
+    push dx
+    mov dx, ax
+    add dl, 30h
+    mov ah, 2
+    int 21h
+    pop dx
+    pop ax
+    ret
+
+multiplication:
+    ; Performing multiplication
+    mov ah, 9
+    mov dx, offset msgB
+    int 21h
+    mov cx, 0
+    call inputno
+    push dx
+    mov ah, 9
+    mov dx, offset msgC
+    int 21h
+    mov cx, 0
+    call inputno
+    pop bx
+    mov ax, dx
+    mul bx
+    mov dx, ax
+    push dx
+    mov ah, 9
+    mov dx, offset msgH
+    int 21h
+    pop dx
+    mov cx, 10000
+    call view
+    jmp exit
+
+subtraction:
+    ; Performing subtraction
+    mov ah, 9
+    mov dx, offset msgB
+    int 21h
+    mov cx, 0
+    call inputno
+    push dx
+    mov ah, 9
+    mov dx, offset msgC
+    int 21h
+    mov cx, 0
+    call inputno
+    pop bx
+    sub bx, dx
+    mov dx, bx
+    push dx
+    mov ah, 9
+    mov dx, offset msgF
+    int 21h
+    pop dx
+    mov cx, 10000
+    call view
+    jmp exit
+
+division:
+    ; Performing division
+    mov ah, 9
+    mov dx, offset msgB
+    int 21h
+    mov cx, 0
+    call inputno
+    push dx
+    mov ah, 9
+    mov dx, offset msgC
+    int 21h
+    mov cx, 0
+    call inputno
+    pop bx
+    mov ax, bx
+    mov cx, dx
+    mov dx, 0
+    div cx
+    mov dx, ax
+    push dx
+    mov ah, 9
+    mov dx, offset msgG
+    int 21h
+    pop dx
+    mov cx, 10000
+    call view
+    jmp exit
+
+square:
+    ; Performing square
+    mov ah, 9
+    mov dx, offset msgM
+    int 21h
+    mov cx, 0
+    call inputno
+    push dx
+    mov ah, 9
+    mov cx, 0
+    pop bx
+    mov ax, dx
+    mul bx
+    mov dx, ax
+    push dx
+    mov ah, 9
+    mov dx, offset msgI
+    int 21h
+    pop dx
+    mov cx, 10000
+    call view
+    jmp exit
+
+power:
+    ; Performing power
+    ; Enter base
+    lea dx, nl1
+    mov ah, 9
+    mov dx, offset msgJ
+    int 21h
+    mov ah, 1
+    int 21h
+    sub al, 30h
+    mov bl, al
+    mov base, al
+
+    ; Enter power
+    lea dx, nl2
+    mov ah, 9
+    mov dx, offset msgK
+    int 21h
+    mov ah, 1
+    int 21h
+    sub al, 30h
+    mov cl, al
+    dec cl
+    mov ax, 00
+    mov al, base
+
+lbl1:
+    mul bl
+    loop lbl1
+    mov cl, 10
+    div cl
+    add ax, 3030h
+    mov dx, ax
+    mov ah, 02h
+    int 21h
+    mov dl, dh
+    int 21h
+    mov ah, 4ch
+    int 21h
+
+main endp
 end main
-     
-
-
